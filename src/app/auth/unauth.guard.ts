172f604f842889aeefb662c0f88be42aa6e39514
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-09-13 21:56:21 +0200

    :passport_control: add auth an unauth guards

---
+++ src/app/auth/unauth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnauthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
